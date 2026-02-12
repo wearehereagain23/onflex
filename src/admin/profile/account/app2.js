d6496b8343519e531c00b3f186f0f3de18cfd47b
wearehereagain23 <wearehereagain23@gmail.com>
2026-02-12 05:01:59 -0800

    wefvqf

--- src/admin/profile/account/app2.js
+++ src/admin/profile/account/app2.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabase = createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
 
